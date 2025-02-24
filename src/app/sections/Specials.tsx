3c75096fd83619ff5b8358de76fe3731c6f9748e
SamWaku <SamuelWaku1st@gmail.com>
2025-02-24 12:57:10 +0200

    specials

--- src/app/sections/Specials.tsx
+++ src/app/sections/Specials.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Specials() {
-  return <div className=" flex-1  mx-auto container py-6"></div>;
+  return <div className=" flex-1  mx-auto container py-6">Specials</div>;
 }
