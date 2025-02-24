17aa501376d72f0d4432fa1289213726eb9d6a65
SamWaku <SamuelWaku1st@gmail.com>
2025-02-24 13:05:49 +0200

    star

--- src/app/sections/Specials.tsx
+++ src/app/sections/Specials.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Specials() {
-  return <div className="   md:mx-12 md:py-10 md:space-y-10">Specials</div>;
+  return (
+    <div className="   md:mx-12 md:py-10 md:space-y-10">
+      <div className=" text-center text-[#524d41] md:text-4xl lg:text-4xl uppercase md:font-semibold lg:font-semibold">
+        <div>Specials</div>
+      </div>
+    </div>
+  );
 }
