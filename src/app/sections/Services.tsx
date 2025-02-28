c8274269ab580cfbb15b34b364b444cebe21d4cd
SamWaku <SamuelWaku1st@gmail.com>
2025-02-28 22:43:31 +0200

    center2

--- src/app/sections/Services.tsx
+++ src/app/sections/Services.tsx
@@ -4,23 +4,23 @@ import { MoveRight } from "lucide-react";
 
 export default function Services() {
   return (
-    <div className=" md:mx-12 md:py-10 md:space-y-10">
-      <div className=" text-[#524d41] grid md:grid-cols-2 grid-cols-1 text-center">
-        <div className=" md:space-y-2">
-          <p className=" uppercase">Type of Bouquet</p>
-          <p className=" md:text-4xl md:font-semibold">
+    <div className=" md:mx-12 mx-10 py-10  space-y-10">
+      <div className=" text-[#524d41] grid md:grid-cols-2 grid-cols-1 md:text-left text-center">
+        <div className=" space-y-6 md:space-y-2">
+          <p className=" uppercase md:text-lg text-2xl">Type of Bouquet</p>
+          <p className=" text-4xl font-semibold">
             Find your perfect <br />
             flower bouquet
           </p>
         </div>
-        <div className="md:flex-row md:space-y-6">
+        <div className="md:flex-row space-y-6">
           <div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
               libero debitis, harum eos ad dolor dolores eum dolore nemo saepe.
             </p>
           </div>
-          <div className=" md:grid md:grid-cols-2">
+          <div className=" md:mx-0 mx-12 grid md:grid-cols-2">
             <p className=" text-center items-center border md:p-2 bg-[#504d43] text-white font-light">
               Show All
             </p>
@@ -28,7 +28,7 @@ export default function Services() {
           </div>
         </div>
       </div>
-      <div className=" flex md:grid md:grid-cols-3 md:items-center">
+      <div className=" md:space-y-0 space-y-12 grid grid-cols-1 md:grid-cols-3 md:items-center">
         <div className=" relative ">
           <div className=" -rotate-12 absolute md:top">
             <p>Lillies </p>
@@ -44,7 +44,7 @@ export default function Services() {
               priority
             />
           </div>
-          <div className=" md:pt-4 md:absolute md:left-28 md:grid md:grid-cols-2 md:gap-2">
+          <div className="  md:pt-4 absolute left-24 md:left-28 flex md:grid md:grid-cols-2 gap-2">
             <div>
               <p className=" uppercase">Shop Now </p>
             </div>
@@ -67,7 +67,7 @@ export default function Services() {
               priority
             />
           </div>
-          <div className=" md:pt-4 md:absolute md:left-28 md:grid md:grid-cols-2 md:gap-2">
+          <div className="  md:pt-4 absolute left-24 md:left-28 flex md:grid md:grid-cols-2 gap-2">
             <div>
               <p className=" uppercase">Shop Now </p>
             </div>
@@ -90,7 +90,7 @@ export default function Services() {
               priority
             />
           </div>
-          <div className=" md:pt-4 md:absolute md:left-28 md:grid md:grid-cols-2 md:gap-2">
+          <div className="  md:pt-4 absolute left-24 md:left-28 flex md:grid md:grid-cols-2 gap-2">
             <div>
               <p className=" uppercase">Shop Now </p>
             </div>
