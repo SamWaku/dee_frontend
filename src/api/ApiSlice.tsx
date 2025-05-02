bf680b516a7c1320a357b95e1a41652678435d6d
SamWaku <samuelwaku1st@gmail.com>
2025-05-02 15:18:27 +0200

    apislicecreateAPI

--- src/api/ApiSlice.tsx
+++ src/api/ApiSlice.tsx
@@ -1,12 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { headers } from "next/headers";
 
 export const Api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "/",
-    prepareHeaders: (headers) => {
-      return headers;
-    },
   }),
   endpoints: () => ({}),
 });
