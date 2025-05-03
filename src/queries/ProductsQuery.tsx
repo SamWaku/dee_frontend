b9dd1be5bda7ae5307909b6f9bf9f25f467a4ec7
SamWaku <samuelwaku1st@gmail.com>
2025-05-03 08:34:57 +0200

    productsquery

--- src/queries/ProductsQuery.tsx
+++ src/queries/ProductsQuery.tsx
@@ -1,7 +1,18 @@
 import { Api } from "@/api/ApiSlice";
+import build from "next/dist/build";
+
+export interface Products
+{
+    id: number
+    productname: string;
+}
 
 const ProductsQuery = Api.injectEndpoints({
-  endpoints: (builder) => {},
+    endpoints(builder) => ({
+        getProduct: builder.query<Products[], void>({
+            query:() => 'dee/products'
+        })
+    })
 });
 
 export const {} = ProductsQuery;
